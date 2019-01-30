cdf5b719e5fd2d0ea58385dbcc04d261c513f134
natsuod <odanatsumi@OdanoMac-mini.local>
2019-01-30 15:03:40 +0900

    Edit-index.js-not-to-show-user-when-input-nothing

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -20,25 +20,30 @@ function appendNoUser(user) {
   $("#user-search-field").on("keyup", function() {
     var input = $("#user-search-field").val();
 
-    $.ajax({
-      type: 'GET',
-      url: '/users',
-      data: { name: input },
-      dataType: 'json'
-    })
-    .done(function(data) {
+    if (input == ""){
       $("#user-search-result").empty();
-      if (data.length !== 0) {
-        data.forEach(function(user){
-          appendUser(user);
-        });
-      }
-      else {
-        appendNoUser("一致するユーザーがいません");
-      }
-    })
-    fail(function() {
-      alert('ユーザー検索に失敗しました')
-    })
+    }
+    else {
+      $.ajax({
+        type: 'GET',
+        url: '/users',
+        data: { name: input },
+        dataType: 'json'
+      })
+      .done(function(data) {
+        $("#user-search-result").empty();
+        if (data.length !== 0) {
+          data.forEach(function(user){
+            appendUser(user);
+          });
+        }
+        else {
+          appendNoUser("一致するユーザーがいません");
+        }
+      })
+      .fail(function() {
+        alert('ユーザー検索に失敗しました')
+      })
+    }
   });
 });
