30bc24bc2f58038eda63ddc9fecd4212e4789e85
natsuod <odanatsumi@OdanoMac-mini.local>
2019-01-30 14:55:59 +0900

    Add-done-to-get-html

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -8,5 +8,19 @@ $(function() {
       data: { name: input },
       dataType: 'json'
     })
+    .done(function(data) {
+      $("#user-search-result").empty();
+      if (data.length !== 0) {
+        data.forEach(function(user){
+          appendUser(user);
+        });
+      }
+      else {
+        appendNoUser("一致するユーザーがいません");
+      }
+    })
+    fail(function() {
+      alert('ユーザー検索に失敗しました')
+    })
   });
 });
