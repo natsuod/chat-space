7a8f9521aebc6bb937f650536414735cb57f9ce8
natsuod <odanatsumi@OdanoMac-mini.local>
2019-02-05 14:25:21 +0900

    Edit-messages_controller-to-get-difference-chats

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,7 +6,7 @@ def index
   @messages = @group.messages.includes(:user)
   respond_to do |format|
     format.html
-    format.json
+    format.json { @new_messages = @messages.where('id > ?', params[:id]) }
   end
 end
 
